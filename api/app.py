6d6aee2fa903f614c553066329c6e96836acb2ce
Shawn Owen <shawntowen@gmail.com>
2019-09-12 17:44:41 -0500

    add html python bit

--- api/app.py
+++ api/app.py
@@ -4,6 +4,11 @@ import random
 app = Flask(__name__)
 
 
-@app.route('/get-random-number')
+@app.route('/api/get-random-number')
 def get_random_number():
     return(jsonify({'number': int(random.random() * 1000000)}))
+
+
+@app.route('/')
+def get_number_page():
+    return(f"<h1> {int(random.random() * 1000000)}")
