847816e239e04876fe495ed76a3b5bcda94028d5
Shawn Owen <shawntowen@gmail.com>
2019-09-30 02:13:08 -0500

    copy folder

---
+++ java-app-vm/src/main/java/Servlet.java
@@ -0,0 +1,26 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Random;
+
+public class Servlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+
+        try (PrintWriter writer = response.getWriter()) {
+            writer.print("<html>");
+            writer.print("<body>");
+            Random rand = new Random();
+            writer.print(rand.nextInt(1000000));
+            writer.print("</body>");
+            writer.print("</html>");
+        }
+    }
+}
